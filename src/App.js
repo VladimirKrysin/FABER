1389ea70bba452783cd2a3b90982b97107f618cb
Vladimir Krysin <vovakrysinn@gmail.com>
2024-03-31 20:16:23 +0300

    123

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  // const services = fetch("127.0.0.1:3000/haircut-services")
+  // useEffect(() => {
+  //   const fetchDataForPosts = async () => {
+  //     const response = await fetch(
+  //       "127.0.0.1:3000/haircut-services"
+  //     );
+
+  //     let postsData = await response.json();
+  //     console.log(postsData);
+
+  //   };
+
+  //   fetchDataForPosts();
+  // }, []);
   return (
     <div className="App">
       <header className="App-header">
